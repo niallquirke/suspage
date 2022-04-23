6719616414dd90caa00032a6063a83aa57bc137b
Niall Quirke <quirkeniall@gmail.com>
2022-04-23 12:10:31 -0300

    added navbar

--- suspage-client/pages/_app.js
+++ suspage-client/pages/_app.js
@@ -1,7 +1,33 @@
-import '../styles/globals.css'
+import * as React from "react";
+import PropTypes from "prop-types";
+import Head from "next/head";
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import theme from "../components/theme";
+import Navbar from "../components/Navbar";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <title>SusPage</title>
+        <meta
+          name="description"
+          content="A platform for driving business sustainability"
+        />
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Navbar />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
 }
 
-export default MyApp
+MyApp.propTypes = {
+  Component: PropTypes.elementType.isRequired,
+  pageProps: PropTypes.object.isRequired,
+};
