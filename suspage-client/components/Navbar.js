b115b5139747bdab25cc8e81ce3a4297f7b13fa4
Niall Quirke <quirkeniall@gmail.com>
2022-05-05 13:23:25 -0300

    added link to profile page

--- suspage-client/components/Navbar.js
+++ suspage-client/components/Navbar.js
@@ -4,26 +4,14 @@ import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
-import Menu from "@mui/material/Menu";
 import AddRoundedIcon from "@mui/icons-material/AddRounded";
 import Container from "@mui/material/Container";
 import Avatar from "@mui/material/Avatar";
 import Tooltip from "@mui/material/Tooltip";
-import MenuItem from "@mui/material/MenuItem";
 
-const settings = ["Log in", "Sign up"];
+import Link from "./Link";
 
 const Navbar = () => {
-  const [anchorElUser, setAnchorElUser] = React.useState(null);
-
-  const handleOpenUserMenu = (event) => {
-    setAnchorElUser(event.currentTarget);
-  };
-
-  const handleCloseUserMenu = () => {
-    setAnchorElUser(null);
-  };
-
   return (
     <AppBar
       position="static"
@@ -31,14 +19,16 @@ const Navbar = () => {
     >
       <Container maxWidth="md">
         <Toolbar disableGutters>
-          <Typography
-            variant="h6"
-            noWrap
-            component="div"
-            sx={{ flexGrow: 1, fontWeight: "bold" }}
-          >
-            🌍 SusPage
-          </Typography>
+          <Link href="/" underline="none" sx={{ color: "#000", flexGrow: 1 }}>
+            <Typography
+              variant="h6"
+              noWrap
+              component="div"
+              sx={{ fontWeight: "bold" }}
+            >
+              🌍 SusPage
+            </Typography>
+          </Link>
           <Box sx={{ flexGrow: 0, mr: 2 }}>
             <Tooltip title="Add">
               <IconButton sx={{ p: 0 }}>
@@ -47,33 +37,11 @@ const Navbar = () => {
             </Tooltip>
           </Box>
           <Box sx={{ flexGrow: 0, mr: 0 }}>
-            <Tooltip title="Open settings">
-              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt="Remy Sharp" src="profiler.jpg" />
+            <Link href="/profile">
+              <IconButton sx={{ p: 0 }}>
+                <Avatar alt="Niall Quirke" src="profiler.jpg" />
               </IconButton>
-            </Tooltip>
-            <Menu
-              sx={{ mt: "45px" }}
-              id="menu-appbar"
-              anchorEl={anchorElUser}
-              anchorOrigin={{
-                vertical: "top",
-                horizontal: "right",
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: "top",
-                horizontal: "right",
-              }}
-              open={Boolean(anchorElUser)}
-              onClose={handleCloseUserMenu}
-            >
-              {settings.map((setting) => (
-                <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                  <Typography textAlign="center">{setting}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
+            </Link>
           </Box>
         </Toolbar>
       </Container>
